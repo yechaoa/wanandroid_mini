170d1b90fb74c03d415a93880d5fe0b189be4ee7
yechao <best_yc@163.com>
2019-09-17 23:29:08 +0800

    初始化配置

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,7 +7,17 @@ Page({
     motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,13 +25,13 @@ Page({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
+  onLoad: function() {
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -50,5 +60,15 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
-  }
+  },
+
+  // 切换
+  swiperChange: function(e) {
+    console.log(e)
+  },
+
+  // 点击
+  swipClick: function(e) {
+    console.log(e)
+  },
 })
---
+++ utils/network.js
@@ -0,0 +1,115 @@
+/**
+ * url:请求的url
+ * params:请求参数
+ * message:loading提示信息
+ * success:成功的回调
+ * fail:失败的回调
+ */
+const app = getApp()
+
+function postRequest(url, params, success, fail) {
+     this.postRequestLoading(url, params, "", success, fail)
+}
+
+function postRequestLoading(url, params, message, success, fail) {
+     if (message != "") {
+          wx.showLoading({
+               title: message,
+          })
+     }
+     if (wx.getStorageSync('wx_unionId') && wx.getStorageSync('wx_unionId') != '' && url != (app.globalData.baseUrl + 'acustomer/redPacket/authorize') && url != (app.globalData.baseUrl + 'acustomer/index/reqIndex')) {
+          params.unionId = wx.getStorageSync('wx_unionId');
+     }
+     if (wx.getStorageSync('wx_openId') && wx.getStorageSync('wx_openId') != '' && url != (app.globalData.baseUrl + 'acustomer/redPacket/authorize') && url != (app.globalData.baseUrl + 'acustomer/index/reqIndex')) {
+          params.openId = wx.getStorageSync('wx_openId');
+     }
+     if (wx.getStorageSync('city') && wx.getStorageSync('city') != '' && url != (app.globalData.baseUrl + 'acustomer/index/chooseCity') && url != (app.globalData.baseUrl + 'acustomer/order/reqSaveCouponCode')) {
+          params.city = wx.getStorageSync('city');
+     }
+     const postRequestTask = wx.request({
+          url: url,
+          data: params,
+          header: {
+               'content-type': 'application/x-www-form-urlencoded',
+               'cookie': wx.getStorageSync('Set-Cookie')
+          },
+          method: 'POST',
+          success: function (res) {
+               if (message != "") {
+                    wx.hideLoading()
+               }
+               if (res.statusCode == 200) {
+                    if (res.header['Set-Cookie'] && res.header['Set-Cookie'] != ''){
+                         wx.setStorageSync('Set-Cookie', res.header['Set-Cookie'])
+                    }
+                    success(res.data)
+               } else {
+                    fail(res)
+               }
+          },
+          fail: function (res) {
+               if (message != "") {
+                    wx.hideLoading()
+               }
+               fail(res)
+          }
+     })
+
+}
+
+function getRequest(url, params, success, fail) {
+     this.getRequestLoading(url, params, "", success, fail)
+}
+
+function getRequestLoading(url, params, message, success, fail) {
+     if (message != "") {
+          wx.showLoading({
+               title: message,
+          })
+     }
+     const getRequestTask = wx.request({
+          url: url,
+          data: params,
+          header: {
+               'Content-Type': 'application/json',
+               'cookie': wx.getStorageSync('Set-Cookie')
+          },
+          method: 'GET',
+          success: function (res) {
+               if (message != "") {
+                    wx.hideLoading()
+               }
+               if (res.statusCode == 200) {
+                    if (res.header['Set-Cookie'] && res.header['Set-Cookie'] != '') {
+                         wx.setStorageSync('Set-Cookie', res.header['Set-Cookie'])
+                    }
+                    success(res.data)
+               } else {
+                    fail(res)
+               }
+          },
+          fail: function (res) {
+               if (message != "") {
+                    wx.hideLoading()
+               }
+               fail(res)
+          }
+     })
+}
+
+function abortPostRequest(url, params, success, fail) {
+     postRequestTask.abort()
+}
+
+function abortGetRequest(url, params, success, fail) {
+     getRequestTask.abort()
+}
+
+module.exports = {
+     postRequest: postRequest,
+     postRequestLoading: postRequestLoading,
+     getRequest: getRequest,
+     getRequestLoading: getRequestLoading,
+     abortPostRequest: abortPostRequest,
+     abortGetRequest: abortGetRequest
+}
